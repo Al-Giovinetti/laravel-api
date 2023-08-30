5cb17bad0715062edbb71db510b468cb8eddce43
Al-Giovinetti <giovinettialessio@gmail.com>
2023-08-30 16:22:36 +0200

    Creata nuova rotta e Controller per Api

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\ProjectController as ApiProjectController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +17,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::name('api')->group(function(){
+    Route::get('/projects',[ApiProjectController::class,'index'])->name('projects.index');
 });
